af1bf4166c1ed7b1c59b1b19a4df3738cac98039
Ryan Pham <ryanpham29@gmail.com>
2025-06-01 21:26:03 +1000

    Finished code for navbar

---
+++ scripts/navbar.js
@@ -0,0 +1,7 @@
+function openMenu() {
+    document.getElementById("mobileMenu").classList.toggle("open");
+}
+
+function closeMenu() {
+    document.getElementById("mobileMenu").classList.remove("open");
+}
